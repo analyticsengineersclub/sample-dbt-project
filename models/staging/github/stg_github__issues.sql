f619cc4e9647dfd4ba42d503da39e9f878fa1180
Claire Carroll <carroll.claire.e@gmail.com>
2021-09-01 23:01:28 +0000

    Add staging models

---
+++ models/staging/github/stg_github__issues.sql
@@ -0,0 +1,29 @@
+with source as (
+
+    select * from {{ source('github', 'issue') }}
+
+),
+
+renamed as (
+
+    select
+        id as issue_ud,
+        _fivetran_synced,
+        body,
+        closed_at,
+        created_at,
+        locked,
+        milestone_id,
+        number,
+        pull_request,
+        repository_id,
+        state,
+        title,
+        updated_at,
+        user_id
+
+    from source
+
+)
+
+select * from renamed
