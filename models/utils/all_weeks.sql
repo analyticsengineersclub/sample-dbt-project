6e563381275d5054c234ddbd4c75e68415b40756
Claire Carroll <carroll.claire.e@gmail.com>
2021-09-16 23:28:40 +0000

    Tidy up

--- models/utils/all_weeks.sql
+++ models/utils/all_weeks.sql
@@ -1,5 +1,11 @@
+with generated as (
 {{ dbt_utils.date_spine(
     datepart="week",
     start_date="cast('2021-01-03' as date)",
     end_date="cast('2021-10-01' as date)"
 ) }}
+)
+
+select
+    cast(date_week as timestamp) as date_week
+from generated
