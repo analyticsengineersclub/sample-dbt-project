780dc3a27076e21a8d3fd206528ebfde798eae3e
Claire Carroll <carroll.claire.e@gmail.com>
2021-10-14 19:31:57 +0000

    Commit that has a mistake in it

--- models/utils/all_weeks.sql
+++ models/utils/all_weeks.sql
@@ -1,7 +1,7 @@
 with generated as (
     {{ dbt_utils.date_spine(
         datepart="week",
-        start_date="cast('2021-01-03' as date)",
+        start_date="cast('2020-12-27' as date)",
         end_date="cast('2021-10-01' as date)"
     ) }}
 )
